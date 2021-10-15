51b9656f2b30ce6fde62088142d7384d791d9335
kbdors <78949392+kbdors@users.noreply.github.com>
2021-10-15 15:15:12 +0200

    Added css- and js- drafts.

---
+++ js/lor.js
@@ -0,0 +1,10 @@
+﻿// Вспомогательные функции (БОР)
+// ----------------
+// Случайное число между min и max
+function getRandomFloat(min, max) {
+	return Math.random() * (max - min) + min;
+}
+// Случайное целое между min и max
+function getRandomInt(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
---
+++ js/watm.js
@@ -0,0 +1,18 @@
+﻿// Логирование
+// ----------------
+function add_logging(msg) {
+	var log_box = document.getElementById("JournalLogging");
+	log_box.innerHTML += "<br />" + msg;
+	log_box.scrollTop = log_box.scrollHeight;
+}
+function clear_logging() {
+	document.getElementById('JournalLogging').innerHTML = "";
+}
+
+// Привязка обработчиков к элементам
+// ----------------
+document.addEventListener('DOMContentLoaded', function(event) {
+		// Область логирования
+		document.getElementById("btnClearLog").onclick = clear_logging;
+	}
+);
